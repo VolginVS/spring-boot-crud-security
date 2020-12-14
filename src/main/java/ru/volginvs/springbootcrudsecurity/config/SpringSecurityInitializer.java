b62ab16e589c7a3b0e749637fca0eedf4de38e7d
Vsevolod Volgin <volginvs@list.ru>
2020-12-14 21:26:04 +0300

    Complete task

--- src/main/java/ru/volginvs/springbootcrudsecurity/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package ru.volginvs.springbootcrudsecurity.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
-}
